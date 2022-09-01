24458d762a62e80dd8ef905ae0aff05b63d47f0e
crawlic-stud <nikitosik0726@gmail.com>
2022-09-01 11:24:30 +0300

    bot ready to run (i hope)

---
+++ bot.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $1 = build ]
+then
+    pip install -r requirements.txt
+    echo "bot setup done"
+elif [ $1 = run ] 
+then
+    python ./src/main.py
+    echo "bot started"
+elif [ $1 = stop ]
+then
+    pkill -f ./src/main.py
+    echo "bot stopped"
+else
+    echo $1 - "unknown command"
+fi
